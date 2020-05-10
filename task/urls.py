f804683cd644816e05c831d28ed97905e2b5e035
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2020-05-10 16:05:17 -0500

    identacion arreglada

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('', TaskView.as_view(), name='tasks_list_url'),
     path('<str:id>/complete/', TaskComplete.as_view()),
